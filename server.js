ac2f4c33d860eda00e06a81249ee1f93e1e50695
alanam79 <halo2382001@gmail.com>
2022-06-08 14:24:59 -0400

    Add Heroku

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+
+const PORT = process.env.PORT || 3001;
 const app = express();
 const { notes } = require('./db/db.json');
 
@@ -6,10 +8,13 @@ const { notes } = require('./db/db.json');
 
 
 app.get('/api/notes', (req, res) => {
-  res.json(notes);
+  let results = notes;
+  console.log(req.query)
+  res.json(results);
 });
 
 
-app.listen(3001, () => {
-    console.log(`API server now on port 3001!`);
-  });
+// server
+app.listen(PORT, () => {
+  console.log(`API server now on port ${PORT}!`);
+});
